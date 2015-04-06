1f9c00ceca15f0661dc62223f4b874bdc44c4a8b
James Bloom <jamesdbloom@gmail.com>
2015-04-06 19:51:10 +0100

    Upgrading MockServer to 3.9.3

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,7 +53,7 @@ module.exports = function (grunt) {
 
     grunt.registerTask('download_jar', 'Download latest MockServer jar version', function () {
         var done = this.async();
-        require('./downloadJar').downloadJar('3.9.1').then(function () {
+        require('./downloadJar').downloadJar('3.9.3').then(function () {
             done(true);
         }, function () {
             done(false);
