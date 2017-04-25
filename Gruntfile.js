124bba5259f19dc9fc2f6531f438bd7becf4c2c7
James Bloom <jamesdbloom@gmail.com>
2017-04-25 22:30:44 +0100

    trying to debug build

--- Gruntfile.js
+++ Gruntfile.js
@@ -79,6 +79,6 @@ module.exports = function (grunt) {
 
     grunt.registerTask('test', ['start_mockserver', 'nodeunit:started', 'stop_mockserver', 'nodeunit:stopped']);
 
-    grunt.registerTask('default', ['exec', 'deleted_jars', 'download_jar', 'jshint', 'test']);
-    grunt.registerTask('travis', ['default']);
+    grunt.registerTask('default', ['exec:stop_existing_mockservers', 'travis']);
+    grunt.registerTask('travis', ['deleted_jars', 'download_jar', 'jshint', 'test']);
 };
