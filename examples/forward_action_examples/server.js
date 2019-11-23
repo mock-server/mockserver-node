be5f7a91e247ec3f93d0fde4ff75c69328181d13
James Bloom <733179+jamesdbloom@users.noreply.github.com>
2019-11-23 00:59:31 +0000

    added more forward examples

--- examples/forward_action_examples/server.js
+++ examples/forward_action_examples/server.js
@@ -40,6 +40,111 @@ function forwardRequestInHTTPS() {
     );
 }
 
+function forwardOverriddenRequest() {
+    var mockServerClient = require('mockserver-client').mockServerClient;
+    mockServerClient("localhost", 1080).mockAnyResponse({
+        "httpRequest": {
+            "path": "/some/path"
+        },
+        "httpOverrideForwardedRequest": {
+            "httpRequest": {
+                "path": "/some/other/path",
+                "headers": {
+                    "Host": ["target.host.com"]
+                }
+            }
+        }
+    }).then(
+        function () {
+            console.log("expectation created");
+        },
+        function (error) {
+            console.log(error);
+        }
+    );
+}
+
+function forwardOverriddenRequestAndChangeReponse() {
+    var mockServerClient = require('mockserver-client').mockServerClient;
+    mockServerClient("localhost", 1080).mockAnyResponse({
+        "httpRequest": {
+            "path": "/some/path"
+        },
+        "httpOverrideForwardedRequest": {
+            "httpRequest": {
+                "path": "/some/other/path",
+                "headers": {
+                    "Host": ["target.host.com"]
+                }
+            },
+            "httpResponse": {
+                "body": "some_overridden_body"
+            }
+        }
+    }).then(
+        function () {
+            console.log("expectation created");
+        },
+        function (error) {
+            console.log(error);
+        }
+    );
+}
+
+function forwardOverriddenRequestAndChangeHostAndPort() {
+    var mockServerClient = require('mockserver-client').mockServerClient;
+    mockServerClient("localhost", 1080).mockAnyResponse({
+        "httpRequest": {
+            "path": "/some/path"
+        },
+        "httpOverrideForwardedRequest": {
+            "httpRequest": {
+                "path": "/some/other/path",
+                "headers": {
+                    "Host": ["any.host.com"]
+                },
+                "socketAddress": {
+                    "host": "target.host.com",
+                    "port": 1234,
+                    "scheme": "HTTPS"
+                }
+            }
+        }
+    }).then(
+        function () {
+            console.log("expectation created");
+        },
+        function (error) {
+            console.log(error);
+        }
+    );
+}
+
+function forwardOverriddenRequestWithDelay() {
+    var mockServerClient = require('mockserver-client').mockServerClient;
+    mockServerClient("localhost", 1080).mockAnyResponse({
+        "httpRequest": {
+            "path": "/some/path"
+        },
+        "httpOverrideForwardedRequest": {
+            "httpRequest": {
+                "path": "/some/other/path",
+                "headers": {
+                    "Host": ["target.host.com"]
+                }
+            },
+            "delay": {"timeUnit": "SECONDS", "value": 20}
+        }
+    }).then(
+        function () {
+            console.log("expectation created");
+        },
+        function (error) {
+            console.log(error);
+        }
+    );
+}
+
 function javascriptTemplatedForward() {
     var mockServerClient = require('mockserver-client').mockServerClient;
     mockServerClient("localhost", 1080).mockAnyResponse({
